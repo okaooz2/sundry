8335e0bf7bb789d9160bc10135989e6a93d03118
okaooz2 <okaooz2@163.com>
2018-03-04 12:04:42 +0800

    0

--- js/渗透问题_1.js
+++ js/渗透问题_1.js
@@ -8,6 +8,7 @@ $("form").on("submit", function(event) {
     // 根据输入数据创建表格
     var length = $("#length", this).val(); length = parseInt(length);
     var width = $("#width", this).val(); width = parseInt(width);
+    var speed = $("#speed", this).val(); speed = parseInt(speed);
     var $table = $("<table border='1'></table>");
 
     for(var i=0; i<width; ++i) {
@@ -27,7 +28,7 @@ $("form").on("submit", function(event) {
         permeated_class: "is-permeated",
         per_topoint_class: "permeated-endpoint",
         path_class: "is-path",
-        timeout: 300
+        timeout: speed
     });
     connection.work();
 });
